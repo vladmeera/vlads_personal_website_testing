b22b9c5a041c54e228aadd055bc119ab0c367085
JorkOwa <mirgorodvld@gmail.com>
2024-10-29 13:09:27 -0400

    Change the entire website

---
+++ cypress.json/cypress/plugins/index.js
@@ -0,0 +1,10 @@
+{
+  "file": "cypress.json",
+  "content": `{
+  "baseUrl": "http://localhost:3000",
+  "fixturesFolder": "cypress/fixtures",
+  "integrationFolder": "cypress/integration",
+  "pluginsFile": "cypress/plugins/index.js",
+  "supportFile": "cypress/support/index.js"
+}`
+}
