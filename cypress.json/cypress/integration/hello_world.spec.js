a696c6fad931bd212e8ef22d65026bbd8ebae785
JorkOwa <mirgorodvld@gmail.com>
2024-10-30 08:47:25 -0400

    Delete cypress tests

--- cypress.json/cypress/integration/hello_world.spec.js
+++
@@ -1,9 +0,0 @@
-{
-  "baseUrl": "http://localhost:3000",
-  "viewportWidth": 1280,
-  "viewportHeight": 720,
-  "integrationFolder": "cypress/integration",
-  "fixturesFolder": "cypress/fixtures",
-  "pluginsFile": "cypress/plugins/index.js",
-  "supportFile": "cypress/support/index.js"
-}
--- cypress.json/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-{
-  "baseUrl": "http://localhost:3000",
-  "viewportWidth": 1280,
-  "viewportHeight": 720,
-  "fixturesFolder": "cypress/fixtures",
-  "integrationFolder": "cypress/integration",
-  "pluginsFile": "cypress/plugins/index.js",
-  "supportFile": "cypress/support/index.js"
-}
